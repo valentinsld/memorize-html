de4cd43e2c2c7765e78c1bdbdb8f21d1ad3d521c
valentinsld <valentin.salaud@gmail.com>
2022-03-02 22:20:58 +0100

    💄 init style titles, card, buttons

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1>Test</h1>
+  <div class="container">
+    <h1>Principale title</h1>
+    <h2>Secondary title</h2>
+    <h3>Third title</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porttitor eros nec urna feugiat tempor. <a href="#">Integer vitae</a> lacinia odio. Mauris auctor laoreet metus, quis fringilla eros elementum sit amet. Proin at imperdiet quam. Aenean ante ipsum, vehicula vel mollis quis, pulvinar sed justo.
+    </p>
+
+    <button class="btn">
+      Primary button
+    </button>
+    <button class="btn -secondary">
+      Secondary button
+    </button>
+
+    <div class="card" style="width: 200px; height: 300px">
+      <p>Text</p>
+    </div>
+  </div>
 </template>
 
 <script>
