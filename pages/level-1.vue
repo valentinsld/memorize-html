3f1913b0ad083dd9b37f5afc8592d90c33ef01aa
valentinsld <valentin.salaud@gmail.com>
2022-03-08 17:35:49 +0100

    ✨ level-1: add QuizzComponent

--- pages/level-1.vue
+++ pages/level-1.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Level 1</h1>
+  <div>
+    <Quizz
+      :data="dataEN"
+      :level="1"
+      :number-question="5"
+    />
+  </div>
 </template>
 
 <script>
+import dataEN from '../_assets/datas/en.json'
+
 export default {
-  name: 'Level1'
+  name: 'Level1',
+  data () {
+    return {
+      dataEN: Object.values(dataEN)
+    }
+  }
 }
 </script>
