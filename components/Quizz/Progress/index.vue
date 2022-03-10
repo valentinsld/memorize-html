77ab3d12ac750115f9d47a6e06cfe0c28b8d27bd
valentinsld <valentin.salaud@gmail.com>
2022-03-10 15:27:05 +0100

    💄 QuizzProgress

--- components/Quizz/Progress/index.vue
+++ components/Quizz/Progress/index.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
-    {{ current }} / {{ max }}
+    <div
+      class="progress"
+      :class="{
+        '-hasLot': max > 20
+      }"
+    >
+      <span
+        v-for="index in max"
+        :key="index"
+        class="progress__item"
+        :class="{
+          '-passed': index <= current
+        }"
+      />
+    </div>
+    <p class="progressMobile">
+      {{ current }} / {{ max }}
+    </p>
   </div>
 </template>
 
 <script>
+import './style.less'
+
 export default {
   name: 'QuizzProgress',
   props: {
