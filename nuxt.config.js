ee60e1e18ebd0cc103b3742a41b817906e40d5db
valentinsld <valentin.salaud@gmail.com>
2022-03-16 20:52:45 +0100

    ✨ add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,19 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'Educational games to learn HTML tags. Because we don’t use tags well enough.', name: 'Educational games to learn HTML tags. Because we don’t use tags well enough.', content: 'Educational games to learn HTML tags. Because we don’t use tags well enough.' },
+      { name: 'format-detection', content: 'telephone=no' },
+
+      { name: 'msapplication-TileColor', content: '#2b5797' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#777dfd' }
     ]
   },
 
