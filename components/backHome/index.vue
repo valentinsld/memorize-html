e420bdd246017f245c2b7f7ce08bc4c79b88bd5c
valentinsld <valentin.salaud@gmail.com>
2022-03-17 13:41:50 +0100

    ✨ backHome: add popup

--- components/backHome/index.vue
+++ components/backHome/index.vue
@@ -1,9 +1,33 @@
 <!-- eslint-disable vue/no-parsing-error -->
 <template>
   <nav class="backHome">
-    <nuxt-link to="/">
-      < Back to home
+    <nuxt-link v-if="end" to="/">
+      < Back to home Link
     </nuxt-link>
+    <button v-else class="backHome__link" @click="openPopup">
+      < Back to home Popup
+    </button>
+
+    <div class="backHome__popup" :class="{'-open': popup}">
+      <div class="popup card">
+        <h3 class="popup__title">
+          Are you sure to leave the quiz ?
+        </h3>
+        <p class="popup__subtitle">
+          This will delete your progress
+        </p>
+
+        <div class="popup__buttons">
+          <button class="popup__link" @click="closePopup">
+            Continue the quiz
+          </button>
+          <nuxt-link class="popup__button btn" to="/">
+            Leave the quiz
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+    </button>
   </nav>
 </template>
 
@@ -11,6 +35,27 @@
 import './style.less'
 
 export default {
-  name: 'BackHome'
+  name: 'BackHome',
+  props: {
+    end: {
+      type: Boolean,
+      default () {
+        return false
+      }
+    }
+  },
+  data () {
+    return {
+      popup: false
+    }
+  },
+  methods: {
+    openPopup () {
+      this.popup = true
+    },
+    closePopup () {
+      this.popup = false
+    }
+  }
 }
 </script>
